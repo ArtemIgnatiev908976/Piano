7095eb2d363a3db52789c76856f8ddd103484c46
Artem <ignatev.artem.nik@gmail.com>
2021-03-21 14:43:26 +0400

    событие  на клавиатуре

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-let keys = document.querySelectorAll('.key')
 
-keys.forEach(key =>{
+//событие на клавиши
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
-  key.addEventListener('click', playNote);
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.key-w')
+const blackKeys = document.querySelectorAll('.key.key-b')
 
 
-});
+//событие на клик
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
 
 
-function playNote(e){
-    let key =e.target;
 
-    let note = document.getElementById(key.dataset.note);
-    key.classList.add('active');
-    note.currentTime=0;
-    note.play();
-    note.addEventListener('ended', ()=>{
-        key.classList.remove('active');
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0
+    noteAudio.play()
+    key.classList.add('active')
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active')
     })
 }
-
-
 //полноэкранный режим
 
 
@@ -52,3 +65,4 @@ function toggleScreen(){
 //полноэкранный режим
 
 //событие при нажатии на клавиши
+
